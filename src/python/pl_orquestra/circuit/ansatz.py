bba5d0dd44ce8d5381138104e7cc0bf0083edcb9
antalszava <antalszava@gmail.com>
2020-11-05 18:35:21 -0500

    Func with no input

--- src/python/pl_orquestra/circuit/ansatz.py
+++ src/python/pl_orquestra/circuit/ansatz.py
@@ -98,10 +98,15 @@ class PLAnsatz(Ansatz):
             symbols.append(sympy.Symbol("gamma_" + str(i)))
         return symbols
 
-import qiskit
-
 def create_circuit_from_qasm(circuit: str):
     """Creates an Orquestra core Circuit object from an OpenQASM string."""
+    qc = QuantumCircuit.from_qasm_str(circuit)
+    zcircuit = Circuit(qc)
+    save_circuit(zcircuit, "circuit.json")
+
+def create_circuit():
+    """Creates an Orquestra core Circuit object from an OpenQASM string."""
+    circuit = "OPENQASM 2.0;\ninclude ""qelib1.inc"";\nqreg q[2];\ncreg c[2];\nry(0.4) q[0];\nmeasure q[0] -> c[0];\nmeasure q[1] -> c[1];\n"
     qc = QuantumCircuit.from_qasm_str(circuit)
     zcircuit = Circuit(qc)
     save_circuit(zcircuit, "circuit.json")
