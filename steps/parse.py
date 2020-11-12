64b3e948f116d78eaaa84b4a27598d22d3947d40
antalszava <antalszava@gmail.com>
2020-11-12 18:20:03 -0500

    Update qasm string

--- steps/parse.py
+++ steps/parse.py
@@ -18,7 +18,7 @@ def create_circuit_from_qasm(circuit: str):
 
 def create_circuit():
     """Creates an Orquestra core Circuit object from an OpenQASM string."""
-    circuit = "OPENQASM 2.0;\ninclude ""qelib1.inc"";\nqreg q[2];\ncreg c[2];\nry(0.4) q[0];\nmeasure q[0] -> c[0];\nmeasure q[1] -> c[1];\n"
+    circuit = 'OPENQASM 2.0;\ninclude "qelib1.inc";\nqreg q[2];\ncreg c[2];\nry(0.4) q[0];\nmeasure q[0] -> c[0];\nmeasure q[1] -> c[1];\n'
     qc = QuantumCircuit.from_qasm_str(circuit)
     zcircuit = Circuit(qc)
     save_circuit(zcircuit, "circuit.json")
