2315ce4e93000f58464acf6c899148bb2be2bfe4
antalszava <antalszava@gmail.com>
2020-11-26 21:11:00 -0500

    Qulacs device import

--- pennylane_orquestra/__init__.py
+++ pennylane_orquestra/__init__.py
@@ -3,4 +3,5 @@ from ._version import __version__
 from .orquestra_device import OrquestraDevice
 from .forest_device import QeForestDevice
 from .qiskit_device import QeQiskitDevice
+from .qulacs_device import QeQulacsDevice
 from .ibmq_device import QeIBMQDevice
