e0123d2c4e3f43cff2ed1d44395fb38f74134a2e
antalszava <antalszava@gmail.com>
2020-12-01 20:03:33 -0500

    Docstrings, organizing steps tests

--- steps/test_expval.py
+++ steps/test_expval.py
@@ -18,14 +18,18 @@ exact_devices = [
 
 sampling_devices = [
      '{"module_name": "qeforest.simulator", "function_name": "ForestSimulator", "device_name": "wavefunction-simulator", "n_samples": 1000}',
-     '{"module_name": "qeqiskit.simulator", "function_name": "QiskitSimulator", "device_name": "qasm_simulator", "n_samples": 100}',
-     '{"module_name": "qequlacs.simulator", "function_name": "QulacsSimulator", "n_samples": 100}'
+     '{"module_name": "qeqiskit.simulator", "function_name": "QiskitSimulator", "device_name": "qasm_simulator", "n_samples": 1000}',
+     '{"module_name": "qequlacs.simulator", "function_name": "QulacsSimulator", "n_samples": 1000}'
         ]
 
 @pytest.mark.parametrize("backend_specs", exact_devices)
 class TestExpvalExact:
+    """Tests getting the expecation value of circuits on devices that support
+    exact computations."""
 
-    def test_run_circuit_and_get_expval(self, backend_specs, monkeypatch, tmpdir):
+    def test_only_measure_circuit(self, backend_specs, monkeypatch, tmpdir):
+        """Tests that the correct result in obtained for a circuit that only
+        contains measurements."""
         local_list = []
 
         only_measure_qasm = 'OPENQASM 2.0;\ninclude "qelib1.inc";\nqreg q[2];\ncreg c[2];\n'
@@ -39,6 +43,8 @@ class TestExpvalExact:
         assert lst[0][0] == 1
 
     def test_run_circuit_and_get_expval_hadamard(self, backend_specs, monkeypatch, tmpdir):
+        """Tests that the correct result in obtained for a circuit that
+        contains a Hadamard gate."""
         local_list = []
 
         hadamard_qasm = 'OPENQASM 2.0;\ninclude "qelib1.inc";\nqreg q[2];\ncreg c[2];\nh q[0];\n'
@@ -51,7 +57,13 @@ class TestExpvalExact:
         expval.run_circuit_and_get_expval(backend_specs, hadamard_qasm, target_op)
         assert np.isclose(lst[0][0], 0.0)
 
-    def test_run_empty_circuit(self, backend_specs, monkeypatch):
+@pytest.mark.parametrize("backend_specs", sampling_devices)
+class TestExpvalSampling:
+    """Tests getting the expecation value of circuits on sampling devices."""
+
+    def test_only_measure_circuit(self, backend_specs, monkeypatch):
+        """Tests that the correct result in obtained for a circuit that only
+        contains measurements."""
         local_list = []
 
         simple_qasm = 'OPENQASM 2.0;\ninclude "qelib1.inc";\nqreg q[3];\ncreg c[3];\n'
@@ -59,15 +71,14 @@ class TestExpvalExact:
 
         lst = []
 
-        monkeypatch.setattr(expval, "save_list", lambda val, name: lst.append(val))
+        monkeypatch.setattr(expval, "save_list", lambda val, name: lst.append(val[0]))
 
         expval.run_circuit_and_get_expval(backend_specs, simple_qasm, target_op)
-        assert lst[0][0] == 1.0
-
-@pytest.mark.parametrize("backend_specs", sampling_devices)
-class TestExpvalSampling:
+        assert lst[0] == 1.0
 
     def test_run_circuit_and_get_expval_hadamard(self, backend_specs, monkeypatch, tmpdir):
+        """Tests that the correct result in obtained for a circuit that
+        contains a Hadamard gate."""
         local_list = []
 
         hadamard_qasm = 'OPENQASM 2.0;\ninclude "qelib1.inc";\nqreg q[2];\ncreg c[2];\nh q[0];\n'
@@ -80,8 +91,11 @@ class TestExpvalSampling:
         expval.run_circuit_and_get_expval(backend_specs, hadamard_qasm, target_op)
         assert math.isclose(lst[0][0], 0.0, abs_tol=10e-2)
 
+class TestIBMQ:
+    """Test the IBMQ device."""
+
     @pytest.mark.xfail(raises=IBMQProviderError)
-    def test_run_circuit_and_get_expval_identity_ibmq(self, backend_specs, monkeypatch, tmpdir):
+    def test_run_circuit_and_get_expval_identity_ibmq(self, monkeypatch, tmpdir):
         local_list = []
 
         backend_specs = '{"module_name": "qeqiskit.backend", "function_name": "QiskitBackend", "device_name": "ibmq_qasm_simulator", "n_samples": 8192}'
@@ -95,19 +109,3 @@ class TestExpvalSampling:
 
         expval.run_circuit_and_get_expval(backend_specs, simple_qasm, target_op)
         assert math.isclose(lst[0][0], 1.0, abs_tol=10e-2)
-
-    def test_run_empty_circuit(self, backend_specs, monkeypatch):
-        local_list = []
-
-        backend_specs = '{"module_name": "qeforest.simulator", "function_name": "ForestSimulator", "device_name": "wavefunction-simulator", "n_samples": 100}'
-
-        simple_qasm = 'OPENQASM 2.0;\ninclude "qelib1.inc";\nqreg q[3];\ncreg c[3];\n'
-        target_op = '["[Z0]"]'
-
-        lst = []
-
-        monkeypatch.setattr(expval, "save_list", lambda val, name: lst.append(val[0]))
-
-        expval.run_circuit_and_get_expval(backend_specs, simple_qasm, target_op)
-        assert lst[0] == 1.0
-
