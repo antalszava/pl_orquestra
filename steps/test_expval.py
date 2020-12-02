4f6abf6a93e7009cc4844b04e734c8b50a5038e3
antalszava <antalszava@gmail.com>
2020-12-01 20:06:17 -0500

    Test non-active wire measurements

--- steps/test_expval.py
+++ steps/test_expval.py
@@ -27,19 +27,20 @@ class TestExpvalExact:
     """Tests getting the expecation value of circuits on devices that support
     exact computations."""
 
-    def test_only_measure_circuit(self, backend_specs, monkeypatch, tmpdir):
+    @pytest.mark.parametrize("op", ['["[Z0]"]', '["[Z1]"]', '["[Z2]"]'])
+    def test_only_measure_circuit(self, op, backend_specs, monkeypatch, tmpdir):
         """Tests that the correct result in obtained for a circuit that only
         contains measurements."""
         local_list = []
 
         only_measure_qasm = 'OPENQASM 2.0;\ninclude "qelib1.inc";\nqreg q[2];\ncreg c[2];\n'
-        target_op = '["[Z0]"]'
+        op = '["[Z0]"]'
 
         lst = []
 
         monkeypatch.setattr(expval, "save_list", lambda val, name: lst.append(val))
 
-        expval.run_circuit_and_get_expval(backend_specs, only_measure_qasm, target_op)
+        expval.run_circuit_and_get_expval(backend_specs, only_measure_qasm, op)
         assert lst[0][0] == 1
 
     def test_run_circuit_and_get_expval_hadamard(self, backend_specs, monkeypatch, tmpdir):
@@ -48,32 +49,32 @@ class TestExpvalExact:
         local_list = []
 
         hadamard_qasm = 'OPENQASM 2.0;\ninclude "qelib1.inc";\nqreg q[2];\ncreg c[2];\nh q[0];\n'
-        target_op = '["[Z0]"]'
+        op = '["[Z0]"]'
 
         lst = []
 
         monkeypatch.setattr(expval, "save_list", lambda val, name: lst.append(val))
 
-        expval.run_circuit_and_get_expval(backend_specs, hadamard_qasm, target_op)
+        expval.run_circuit_and_get_expval(backend_specs, hadamard_qasm, op)
         assert np.isclose(lst[0][0], 0.0)
 
 @pytest.mark.parametrize("backend_specs", sampling_devices)
 class TestExpvalSampling:
     """Tests getting the expecation value of circuits on sampling devices."""
 
-    def test_only_measure_circuit(self, backend_specs, monkeypatch):
+    @pytest.mark.parametrize("op", ['["[Z0]"]', '["[Z1]"]', '["[Z2]"]'])
+    def test_only_measure_circuit(self, backend_specs, op, monkeypatch):
         """Tests that the correct result in obtained for a circuit that only
         contains measurements."""
         local_list = []
 
         simple_qasm = 'OPENQASM 2.0;\ninclude "qelib1.inc";\nqreg q[3];\ncreg c[3];\n'
-        target_op = '["[Z0]"]'
 
         lst = []
 
         monkeypatch.setattr(expval, "save_list", lambda val, name: lst.append(val[0]))
 
-        expval.run_circuit_and_get_expval(backend_specs, simple_qasm, target_op)
+        expval.run_circuit_and_get_expval(backend_specs, simple_qasm, op)
         assert lst[0] == 1.0
 
     def test_run_circuit_and_get_expval_hadamard(self, backend_specs, monkeypatch, tmpdir):
@@ -82,13 +83,13 @@ class TestExpvalSampling:
         local_list = []
 
         hadamard_qasm = 'OPENQASM 2.0;\ninclude "qelib1.inc";\nqreg q[2];\ncreg c[2];\nh q[0];\n'
-        target_op = '["[Z0]"]'
+        op = '["[Z0]"]'
 
         lst = []
 
         monkeypatch.setattr(expval, "save_list", lambda val, name: lst.append(val))
 
-        expval.run_circuit_and_get_expval(backend_specs, hadamard_qasm, target_op)
+        expval.run_circuit_and_get_expval(backend_specs, hadamard_qasm, op)
         assert math.isclose(lst[0][0], 0.0, abs_tol=10e-2)
 
 class TestIBMQ:
@@ -101,11 +102,11 @@ class TestIBMQ:
         backend_specs = '{"module_name": "qeqiskit.backend", "function_name": "QiskitBackend", "device_name": "ibmq_qasm_simulator", "n_samples": 8192}'
 
         simple_qasm = 'OPENQASM 2.0;\ninclude "qelib1.inc";\nqreg q[2];\ncreg c[2];\n'
-        target_op = '["[Z0]"]'
+        op = '["[Z0]"]'
 
         lst = []
 
         monkeypatch.setattr(expval, "save_list", lambda val, name: lst.append(val))
 
-        expval.run_circuit_and_get_expval(backend_specs, simple_qasm, target_op)
+        expval.run_circuit_and_get_expval(backend_specs, simple_qasm, op)
         assert math.isclose(lst[0][0], 1.0, abs_tol=10e-2)
