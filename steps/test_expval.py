9629ffe8d2a0ca34cead0d6fcbafe063187de1c4
antalszava <antalszava@gmail.com>
2020-12-07 16:55:15 -0500

    Formatting, linting

--- steps/test_expval.py
+++ steps/test_expval.py
@@ -3,17 +3,13 @@ This module contains tests locally checking that the functionality of the
 expval step is correct. Running the test cases requires the related packages to
 be installed locally.
 """
-import pytest
-import numpy as np
-import expval
-
-from qiskit import QiskitError, IBMQ
-from qiskit.providers.ibmq.exceptions import IBMQProviderError
-
-import qeforest
-import json
-import os
 import math
+import os
+
+import pytest
+from qiskit import IBMQ
+
+import expval
 
 exact_devices = [
     '{"module_name": "qeforest.simulator", "function_name": "ForestSimulator", "device_name": "wavefunction-simulator"}',
@@ -39,29 +35,25 @@ class TestExpvalExact:
     exact computations."""
 
     @pytest.mark.parametrize("op", ['["[Z0]"]', '["[Z1]"]', '["[Z2]"]', '["[]"]'])
-    def test_only_measure_circuit(self, op, backend_specs, monkeypatch, tmpdir):
+    def test_only_measure_circuit(self, op, backend_specs, monkeypatch):
         """Tests that the correct result in obtained for a circuit that only
         contains measurements."""
-        local_list = []
+        lst = []
 
         only_measure_qasm = 'OPENQASM 2.0;\ninclude "qelib1.inc";\nqreg q[3];\ncreg c[3];\n'
-
-        lst = []
 
         monkeypatch.setattr(expval, "save_list", lambda val, name: lst.append(val))
 
         expval.run_circuit_and_get_expval(backend_specs, only_measure_qasm, op)
         assert lst[0][0] == 1
 
-    def test_run_circuit_and_get_expval_hadamard(self, backend_specs, monkeypatch, tmpdir):
+    def test_run_circuit_and_get_expval_hadamard(self, backend_specs, monkeypatch):
         """Tests that the correct result in obtained for a circuit that
         contains a Hadamard gate."""
-        local_list = []
+        lst = []
 
         hadamard_qasm = 'OPENQASM 2.0;\ninclude "qelib1.inc";\nqreg q[2];\ncreg c[2];\nh q[0];\n'
         op = '["[Z0]"]'
-
-        lst = []
 
         monkeypatch.setattr(expval, "save_list", lambda val, name: lst.append(val))
 
@@ -77,26 +69,22 @@ class TestExpvalSampling:
     def test_only_measure_circuit(self, backend_specs, op, monkeypatch):
         """Tests that the correct result in obtained for a circuit that only
         contains measurements."""
-        local_list = []
+        lst = []
 
         simple_qasm = 'OPENQASM 2.0;\ninclude "qelib1.inc";\nqreg q[3];\ncreg c[3];\n'
-
-        lst = []
 
         monkeypatch.setattr(expval, "save_list", lambda val, name: lst.append(val[0]))
 
         expval.run_circuit_and_get_expval(backend_specs, simple_qasm, op)
         assert lst[0] == 1.0
 
-    def test_run_circuit_and_get_expval_hadamard(self, backend_specs, monkeypatch, tmpdir):
+    def test_run_circuit_and_get_expval_hadamard(self, backend_specs, monkeypatch):
         """Tests that the correct result in obtained for a circuit that
         contains a Hadamard gate."""
-        local_list = []
+        lst = []
 
         hadamard_qasm = 'OPENQASM 2.0;\ninclude "qelib1.inc";\nqreg q[2];\ncreg c[2];\nh q[0];\n'
         op = '["[Z0]"]'
-
-        lst = []
 
         monkeypatch.setattr(expval, "save_list", lambda val, name: lst.append(val))
 
@@ -106,6 +94,7 @@ class TestExpvalSampling:
 
 @pytest.fixture
 def token():
+    """Get the IBMQX test token."""
     t = os.getenv("IBMQX_TOKEN_TEST", None)
 
     if t is None:
@@ -119,16 +108,14 @@ class TestIBMQ:
     """Test the IBMQ device."""
 
     @pytest.mark.parametrize("op", ['["[Z0]"]', '["[Z1]"]', '["[Z2]"]', '["[]"]'])
-    def test_run_circuit_and_get_expval_simple_ibmq(self, token, op, monkeypatch, tmpdir):
+    def test_run_circuit_and_get_expval_simple_ibmq(self, token, op, monkeypatch):
         """Test running an empty circuit."""
-        local_list = []
+        lst = []
 
         IBMQ.enable_account(token)
         backend_specs = '{"module_name": "qeqiskit.backend", "function_name": "QiskitBackend", "device_name": "ibmq_qasm_simulator", "n_samples": 8192}'
 
         simple_qasm = 'OPENQASM 2.0;\ninclude "qelib1.inc";\nqreg q[3];\ncreg c[3];\n'
-
-        lst = []
 
         monkeypatch.setattr(expval, "save_list", lambda val, name: lst.append(val))
 
